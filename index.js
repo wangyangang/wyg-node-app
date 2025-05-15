d44638ffebea5e57b9b9818ce2699e39db47478a
wangyangang <yangangwang1991@gmail.com>
2025-05-15 17:46:49 +0800

    修改api接口

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 
 const app = express()
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
 
